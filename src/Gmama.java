6657a55ed1bcf2a352894d4d23aa12d27d72eb24
dfg5dfg4 <1536393150@qq.com>
2017-11-24 23:56:38 +0800

    哈哈

---
+++ src/Gmama.java
@@ -0,0 +1,10 @@
+class Gmama{
+    public static void main(String[] args) {
+        HeHeList gg = new HeHeList();
+gg.add(1);gg.add(2);gg.add(3);
+        System.out.println(gg.size());
+gg.delete(2);gg.size();
+        System.out.println(gg.size());
+
+    }
+}
